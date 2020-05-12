73f6825ad55a0afb4ad8d7d071c1c73a3c88f9f3
kfpersson <kfpersson@gmail.com>
2020-05-12 11:37:14 +0200

    Create get-adcomputer_oslo_1.0.ps1

---
+++ get-adcomputer_oslo_1.0.ps1
@@ -0,0 +1,10 @@
+$comp = get-adcomputer -filter * -properties *
+$list = @{}
+foreach ($i in $comp)
+{
+ $result=$null
+ try {$result=Test-Connection -ComputerName $i.name -count 1 -ea stop}
+ catch {$list += @{$i.name = "DOWN"}}
+ if ($result) {$list += @{$i.name = "UP"}}
+}
+$list | Select @{l='Server';e={$server}},@{l='Status';e={$status}}
